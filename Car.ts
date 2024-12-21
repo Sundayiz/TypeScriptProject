6e71aa69df0f7a3a01a50fd3614efaf8ac8a8bd4
izuoba sunday <sundayizuoba9@gmail.com>
2024-12-21 08:27:10 +0100

    uncompleted

---
+++ Car.ts
@@ -0,0 +1,14 @@
+class Car implements Vehicle {
+  make: string;
+  model: string;
+  year: number;
+
+  constructor(make: string, model: string, year: number) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+  }
+  start(): void {
+    console.log("Car engine start");
+  }
+}
---
+++ main.ts
@@ -0,0 +1,3 @@
+const myCar = new Car("Toyota", "Corolla", 2021);
+console.log(`Car details: ${myCar.make} ${myCar.model} ${myCar.year}`);
+myCar.start();
